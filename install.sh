1b1eb3c9590409c3ee227eb8fe1a3d6ca82772f3
Seif Kamal <seif.kamal@preply.com>
2026-03-28 13:44:52 +0000

    Add Claude settings

--- install.sh
+++ install.sh
@@ -3,6 +3,6 @@ SCRIPT_DIR="$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" && pwd)"
 brew bundle
 ln -svn ${SCRIPT_DIR}/kitty ~/.config/kitty
 ln -svn ${SCRIPT_DIR}/nvim ~/.config/nvim
-ln -svn ${SCRIPT_DIR}/claude/statusline.sh ~/.claude/statusline.sh
+ln -svn ${SCRIPT_DIR}/claude/* ~/.claude/
 ln -svn ${SCRIPT_DIR}/.zshrc ~/.zshrc
 source git/install.sh
